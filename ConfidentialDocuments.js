3468aaabc4f24e885338068ac2afd8098b0d0631
SharePointSan <NL12143@hotmail.com>
2017-11-26 00:17:55 +0100

    Update ConfidentialDocuments.js

--- ConfidentialDocuments.js
+++ ConfidentialDocuments.js
@@ -1,12 +1,13 @@
-﻿// List view – Confidential Documents Sample
+// List view – Confidential Documents Sample
 // Muawiyah Shannak , @MuShannak
+// https://code.msdn.microsoft.com/office/Client-side-rendering-code-97e27fa1
 
 (function () {
 
     // Create object that have the context information about the field that we want to change it's output render 
     var linkFilenameFiledContext = {};
-    linkFilenameFiledContext.Templates = {};
-    linkFilenameFiledContext.Templates.Fields = {
+        linkFilenameFiledContext.Templates = {};
+        linkFilenameFiledContext.Templates.Fields = {
         // Apply the new rendering for LinkFilename field on list view
         "LinkFilename": { "View": linkFilenameFiledTemplate }
     };
@@ -21,7 +22,7 @@ function linkFilenameFiledTemplate(ctx) {
     var confidential = ctx.CurrentItem["Confidential"];
     var title = ctx.CurrentItem["FileLeafRef"];
 
-    // This Regex expression use to delete extension (.docx, .pdf ...) form the file name
+    // This Regex expression use to delete extension (.docx, .pdf ...) from the file name
     title = title.replace(/\.[^/.]+$/, "")
 
     // Check confidential field value
@@ -35,4 +36,3 @@ function linkFilenameFiledTemplate(ctx) {
         return title;
     }
 }
-
